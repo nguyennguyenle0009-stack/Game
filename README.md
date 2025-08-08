a470391d8d5997e9d2536cf79de61c11f352e273
HocITnao <nguyennguyenle0009@gmail.com>
2025-08-08 21:58:44 +0700

    Sự kiện (Hố sát thương, Hồ hồi máu, Ô dịch chuyển) Advanced Mechanics

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Project: học lập trình game.
 
--https://www.youtube.com/watch?v=om59cwR7psI&list=PL_QPQmz5C6WUF-pOQDsbsKbaBZqXj4qSq
+- https://www.youtube.com/watch?v=om59cwR7psI&list=PL_QPQmz5C6WUF-pOQDsbsKbaBZqXj4qSq
 	
 ## Tính năng
 
@@ -8,6 +8,7 @@
 - Thu thập chìa khóa, dùng chìa khóa mở cữa(drop).
 - Thu thập rương báu và kết thúc game(drop).
 - Tạm dừng và tiếp tục.
+- Sự kiện (Hố sát thương, Hồ hồi máu, Ô dịch chuyển) Advanced Mechanics
 
 ## Cập nhật
 
@@ -27,11 +28,14 @@
 
 ### Lỗi 
 
+- Khi người chơi liên tục đi qua hố, họ liên tục bị sát thương và nhận được thông báo
+
 ### Sửa lỗi
 
-### PS
+- Thêm các phản ứng đối tượng
+- chỉ tạo sự kiện một lần để mỗi sự kiện có giá trị boolean và kiểm tra xem sự kiện đã xảy ra trong quá khứ hay chưa
 
-- Không nên tạo một phiên bản mới trong vòng lặp trò chơi trừ khi thực sự cần thiết.
+### PS
 
 ### Comment
 
@@ -41,6 +45,7 @@
 - Nói chuyện với npc.
 - Làm npc quay về phía người chơi khi nói chuyện, chuỗi string xuống 1 dòng.
 - Thiết lập font chữ
+- Sự kiện (Hố sát thương, Hồ hồi máu, Ô dịch chuyển) Advanced Mechanics
 
 ### So sánh
 
@@ -51,5 +56,11 @@
 | Dễ mở rộng                | ✅ Dễ thêm tile mới                            | ❌ Khó                                     |
 | Quản lý tileSize dễ dàng  | ✅ Có                                          | ❌ Không                                   |
 | Viết code                 | ⭐ Ngắn gọn                                    | ❌ Dài dòng, trùng lặp                     |
+
+### Ý tưởng
+
+- Will you show how to add events to objects too? Also, when the player keeps walking through the pit they constantly take damage and get message, can we make it so event only happens if they leave and go back through the pit?
+- We will add object reactions eventually (we've already done it in the first half but it will be handled in more object oriented way in this second half). For the event keeps happening when you are going to the same direction, there are two ways to fix. The one is creating some margin so the event won't happen again until player moves certain amount of pixels from where the event happened. This will be implemented when we add stairs in the future. The second one is  making the event one time only so each event has boolean value and check if the event has already happened in the past or not. For the second method, you'll probably want to handle it by creating an event tile class. Maybe I will make a video for this.
+- I created a method to take in a direction and move the player backwards if they step on a pit. You could also create some sort of timer where the player is invincible for a second or so after taking damage
 
 
--- src/game/main/EventHandler.java
+++ src/game/main/EventHandler.java
@@ -1,65 +1,91 @@
 package game.main;
 
-import java.awt.Rectangle;
-
 public class EventHandler {
 	
 	GamePanel gp;
-	Rectangle eventRect;
-	int eventRectDefaultX, eventRectDefaultY;
+	EventRect eventRect[][];
 	
+	int previousEventX, previousEventY;
+	boolean canTounchEvent = true;
 	
 	public EventHandler (GamePanel gp) {
 		this.gp = gp;
 		
-		eventRect = new Rectangle();
-		eventRect.x = 23;
-		eventRect.y = 23;
-		eventRect.width = 2;
-		eventRect.height = 2;
-		eventRectDefaultX = eventRect.x;
-		eventRectDefaultY = eventRect.y;
+		eventRect = new EventRect[gp.maxWorldCol][gp.maxWorldRow];
+		
+		int col = 0;
+		int row = 0;
+		while(col < gp.maxWorldCol && row < gp.maxWorldRow) {
+			eventRect[col][row] = new EventRect();
+			eventRect[col][row].x = 23;
+			eventRect[col][row].y = 23;
+			eventRect[col][row].width = 2;
+			eventRect[col][row].height = 2;
+			eventRect[col][row].eventRectDefaultX = eventRect[col][row].x;
+			eventRect[col][row].eventRectDefaultY = eventRect[col][row].y;
+			
+			col++;
+			if (col == gp.maxWorldCol) {
+				col = 0;
+				row++;
+			}
+		}
 	}
 	
 	public void checkEvent() {
 		
-		//Hố sát thương
-		//if (hit(27, 16, "right") == true) { damagePit(gp.dialogueState); }
-		if (hit(27, 16, "right") == true) { teleport(gp.dialogueState); }
-		if (hit(23, 12, "up") == true) { healingPool(gp.dialogueState); };
 		
+		//check if the player character is more than 1 tile away from the last event
+		int xDiastance = Math.abs(gp.player.worldX - previousEventX);
+		int yDiastance = Math.abs(gp.player.worldY - previousEventY);
+		int distance = Math.max(xDiastance, yDiastance);
+		if (distance > gp.tileSize) {
+			canTounchEvent = true;
+		}
+		
+		if (canTounchEvent == true) {
+			if (hit(27, 16, "right") == true) { damagePit( 27, 16, gp.dialogueState); }
+			if (hit(23, 19, "any") == true) { damagePit( 27, 16, gp.dialogueState); }
+			//if (hit(27, 16, "right") == true) { teleport(gp.dialogueState); }
+			if (hit(23, 12, "up") == true) { healingPool( 23, 12, gp.dialogueState); };
+		}
 	}
 	
-	public boolean hit(int eventCol, int eventRow, String reDirection) {
+	public boolean hit(int col, int row, String reDirection) {
 		
 		boolean hit = false;
 		
 		gp.player.solidArea.x = gp.player.worldX + gp.player.solidArea.x;
 		gp.player.solidArea.y = gp.player.worldY + gp.player.solidArea.y;
-		eventRect.x = eventCol * gp.tileSize + eventRect.x;
-		eventRect.y = eventRow * gp.tileSize + eventRect.y;
+		eventRect[col][row].x = col * gp.tileSize + eventRect[col][row].x;
+		eventRect[col][row].y = row * gp.tileSize + eventRect[col][row].y;
 		
-		if (gp.player.solidArea.intersects(eventRect)) {
+		if (gp.player.solidArea.intersects(eventRect[col][row]) && eventRect[col][row].eventDone == false) {
 			if (gp.player.direction.contentEquals(reDirection) || reDirection.contentEquals("any")) {
 				hit = true;
+				
+				previousEventX = gp.player.worldX;
+				previousEventY = gp.player.worldY;
 			}
 		}
 		
 		gp.player.solidArea.x = gp.player.solidAreaDefaultX;
 		gp.player.solidArea.y = gp.player.solidAreaDefaultY;
-		eventRect.x = eventRectDefaultX;
-		eventRect.y = eventRectDefaultY;
+		eventRect[col][row].x = eventRect[col][row].eventRectDefaultX;
+		eventRect[col][row].y = eventRect[col][row].eventRectDefaultY;
 		
 		return hit;
 	}
 	
-	public void damagePit(int gameState) {
+	public void damagePit(int col, int row, int gameState) {
 		gp.gameState = gameState;
 		gp.ui.currentDialogue = "You fail into a pit!";
 		gp.player.life -= 1;
+		//eventRect[col][row].eventDone = true;
+		canTounchEvent = false;
 	}
 	
-	public void healingPool(int gameState) {
+	public void healingPool(int col, int row, int gameState) {
 		if (gp.keyH.enterPresed == true) {
 			gp.gameState = gameState;
 			gp.ui.currentDialogue = "You drink the water.\nYour lift has been recovered";
